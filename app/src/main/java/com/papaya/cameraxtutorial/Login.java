e756365707480ee52bf77efb370cc0975c472641
DocPapaya456 <natsoon2580@gmail.com>
2022-03-28 17:57:38 +0800

    login started

---
+++ app/src/main/java/com/papaya/cameraxtutorial/Login.java
@@ -0,0 +1,13 @@
+package com.papaya.cameraxtutorial;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import io.realm.Realm;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+    }
+}
