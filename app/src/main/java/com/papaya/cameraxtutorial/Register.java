b60426f92dc0e1453ac91944c3d939884e74b6db
DocPapaya456 <natsoon2580@gmail.com>
2022-08-15 21:39:00 +0800

    added register functionality

--- app/src/main/java/com/papaya/cameraxtutorial/Register.java
+++ app/src/main/java/com/papaya/cameraxtutorial/Register.java
@@ -1,15 +1,29 @@
 package com.papaya.cameraxtutorial;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
+    EditText newEmail;
+    EditText newPassword;
+    Button registerBtn;
+    Button cancelBtn;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //Initialise buttons and textbox
+        newEmail = findViewById(R.id.newEmail);
+        newPassword = findViewById(R.id.newPassword);
+        registerBtn = findViewById(R.id.registerBtn);
+        cancelBtn = findViewById(R.id.cancelBtn);
+
+
 
     }
 }
