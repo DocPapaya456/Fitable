4ab8686b7f4ea2a93ea9a710b69a8c0c98cac194
DocPapaya456 <natsoon2580@gmail.com>
2022-08-29 18:54:50 +0800

    Update RepCounter.java

--- app/src/main/java/com/papaya/cameraxtutorial/RepCounter.java
+++ app/src/main/java/com/papaya/cameraxtutorial/RepCounter.java
@@ -30,6 +30,12 @@ public class RepCounter {
             PoseLandmark rk = null;
             PoseLandmark la = null;
             PoseLandmark ra = null;
+            PoseLandmark ls = null;
+            PoseLandmark rs = null;
+            PoseLandmark lw = null;
+            PoseLandmark rw = null;
+            PoseLandmark le = null;
+            PoseLandmark re = null;
             try {
                 if (landmarks.get(PoseLandmark.LEFT_HIP).getInFrameLikelihood() > 0.5) {
                     lh = landmarks.get(PoseLandmark.LEFT_HIP);
@@ -49,14 +55,36 @@ public class RepCounter {
                 if (landmarks.get(PoseLandmark.RIGHT_ANKLE).getInFrameLikelihood() > 0.5) {
                     ra = landmarks.get(PoseLandmark.RIGHT_ANKLE);
                 }
+                if (landmarks.get(PoseLandmark.LEFT_SHOULDER).getInFrameLikelihood() > 0.5) {
+                    ls = landmarks.get(PoseLandmark.LEFT_SHOULDER);
+                }
+                if (landmarks.get(PoseLandmark.RIGHT_SHOULDER).getInFrameLikelihood() > 0.5) {
+                    rs = landmarks.get(PoseLandmark.RIGHT_SHOULDER);
+                }
+                if (landmarks.get(PoseLandmark.LEFT_WRIST).getInFrameLikelihood() > 0.5) {
+                    lw = landmarks.get(PoseLandmark.LEFT_WRIST);
+                }
+                if (landmarks.get(PoseLandmark.RIGHT_WRIST).getInFrameLikelihood() > 0.5) {
+                    rw = landmarks.get(PoseLandmark.RIGHT_WRIST);
+                }
+                if (landmarks.get(PoseLandmark.LEFT_ELBOW).getInFrameLikelihood() > 0.5) {
+                    le = landmarks.get(PoseLandmark.LEFT_ELBOW);
+                }
+                if (landmarks.get(PoseLandmark.RIGHT_ELBOW).getInFrameLikelihood() > 0.5) {
+                    re = landmarks.get(PoseLandmark.RIGHT_ELBOW);
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
             switch (type) {
                 case "squats":
                     if (lh != null && rh != null && lk != null && rk != null) {
-                        if (lh.getPosition().y > lk.getPosition().y && rh.getPosition().y > rk.getPosition().y) {
-                            return "down";
+                        if (lh.getPosition().y <= (lk.getPosition().y + 10)) {
+                            if (lh.getPosition().y >= lk.getPosition().y - 10) {
+                                return "down";
+                            } else {
+                                return "hips too low";
+                            }
                         } else {
                             return "up";
                         }
@@ -71,17 +99,5 @@ public class RepCounter {
         return "error";
 
     }
-    static double getAngle(PoseLandmark firstPoint, PoseLandmark midPoint, PoseLandmark lastPoint) {
-        double result =
-                Math.toDegrees(
-                        atan2(lastPoint.getPosition().y - midPoint.getPosition().y,
-                                lastPoint.getPosition().x - midPoint.getPosition().x)
-                                - atan2(firstPoint.getPosition().y - midPoint.getPosition().y,
-                                firstPoint.getPosition().x - midPoint.getPosition().x));
-        result = Math.abs(result); // Angle should never be negative
-        if (result > 180) {
-            result = (360.0 - result); // Always get the acute representation of the angle
-        }
-        return result;
-    }
+
 }
